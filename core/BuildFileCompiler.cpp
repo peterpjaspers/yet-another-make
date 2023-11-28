d0adc708114e766c3d06b004f919b904c278e30e
peterpjaspers <peterpjaspers@gmail.com>
2023-11-28 16:57:56 +0100

    Cleanup BuildFileCompiler

--- core/BuildFileCompiler.cpp
+++ core/BuildFileCompiler.cpp
@@ -43,10 +43,56 @@ namespace YAM {
         }
     }
 
+    std::vector<std::filesystem::path> BuildFileCompiler::expandOutputPaths(
+        BuildFile::Outputs const& outputs,
+        std::vector<std::shared_ptr<FileNode>> const& cmdInputs
+    ) const {
+        std::vector<std::filesystem::path> outputPaths;
+        // TODO: resolve %B etc
+        for (auto const& output : outputs.outputs) {
+            std::filesystem::path outputPath = _baseDir->name() / output.path;
+            outputPaths.push_back(outputPath);
+        }
+        return outputPaths;
+
+    }
+
+    std::shared_ptr<CommandNode> BuildFileCompiler::createCommand(
+        std::vector<std::filesystem::path> const& outputPaths) const
+    {
+        std::shared_ptr<CommandNode> cmdNode;
+        std::filesystem::path cmdName;
+        if (outputPaths.empty()) {
+            cmdName = uniqueCmdName(_context->nodes());
+        } else {
+            cmdName = outputPaths[0] / "__cmd";
+        }
+        std::shared_ptr<Node> node = _context->nodes().find(cmdName);
+        cmdNode = dynamic_pointer_cast<CommandNode>(node);
+        if (cmdNode == nullptr) {
+            cmdNode = std::make_shared<CommandNode>(_context, cmdName);
+        }
+        return cmdNode;
+    }
+
+    void BuildFileCompiler::addCommand(
+        BuildFile::Rule const& rule,
+        std::shared_ptr<CommandNode> const& cmdNode,
+        std::vector<std::shared_ptr<FileNode>> const& cmdInputs,
+        std::vector<std::shared_ptr<GeneratedFileNode>> const& orderOnlyInputs,
+        std::vector<std::filesystem::path> const& outputPaths
+    ) {
+        std::vector<std::shared_ptr<GeneratedFileNode>> outputs = compileOutputs(cmdNode, outputPaths, cmdInputs);
+        std::string script = compileScript(rule.script, cmdInputs, orderOnlyInputs, outputs);
+        cmdNode->script(script);
+        // TODO: initialize cmdNode
+        _commands.push_back(cmdNode);
+    }
+
     std::vector<std::shared_ptr<FileNode>> BuildFileCompiler::compileInput(
         BuildFile::Input const& input,
         std::vector<std::shared_ptr<FileNode>> included
-    ) {
+    ) const {
         std::vector<std::shared_ptr<FileNode>> inputNodes;
         std::shared_ptr<FileNode> fileNode;
         // TODO: use Globber
@@ -64,7 +110,7 @@ namespace YAM {
 
     std::vector<std::shared_ptr<FileNode>> BuildFileCompiler::compileInputs(
         BuildFile::Inputs const& inputs
-    ) {
+    ) const {
         std::vector<std::shared_ptr<FileNode>> inputNodes;
         for (auto const& input : inputs.inputs) {
             std::vector<std::shared_ptr<FileNode>> nodes = compileInput(input, inputNodes);
@@ -78,79 +124,53 @@ namespace YAM {
         std::vector<std::shared_ptr<FileNode>> cmdInputs,
         std::vector<std::shared_ptr<GeneratedFileNode>> orderOnlyInputs,
         std::vector<std::shared_ptr<GeneratedFileNode>> outputs
-    ) {
+    ) const {
         return script.script;
     }
 
     std::vector<std::shared_ptr<GeneratedFileNode>> BuildFileCompiler::compileOutput(
         BuildFile::Output const& output,
         std::vector<std::shared_ptr<FileNode>> const& inputs
-    ) {
+    ) const {
         std::vector<std::shared_ptr<GeneratedFileNode>> outputNodes;
         return outputNodes;
     }
 
     std::vector<std::shared_ptr<GeneratedFileNode>> BuildFileCompiler::compileOutputs(
-        BuildFile::Outputs const& outputs,
+        std::shared_ptr<CommandNode> const& cmdNode,
+        std::vector<std::filesystem::path> const& outputPaths,
         std::vector<std::shared_ptr<FileNode>> const& inputs
-    ) {
+    ) const {
         std::vector<std::shared_ptr<GeneratedFileNode>> outputNodes;
         // TODO: resolve %B etc
-        for (auto const& output : outputs.outputs) {
-            std::shared_ptr<GeneratedFileNode> fileNode;
-            std::filesystem::path fileName = _baseDir->name() / output.path;
-            std::shared_ptr<Node> node = _context->nodes().find(fileName);
+        for (auto const& outputPath : outputPaths) {
+            std::shared_ptr<GeneratedFileNode> outputNode;
+            std::shared_ptr<Node> node = _context->nodes().find(outputPath);
             if (node != nullptr) {
-                fileNode = dynamic_pointer_cast<GeneratedFileNode>(node);
-                if (fileNode == nullptr) throw std::runtime_error("not a generated file node");
+                outputNode = dynamic_pointer_cast<GeneratedFileNode>(node);
+                if (outputNode == nullptr) throw std::runtime_error("not a generated file node");
             } else {
-                fileNode = std::make_shared<GeneratedFileNode>(_context, fileName, nullptr);
+                outputNode = std::make_shared<GeneratedFileNode>(_context, outputPath, cmdNode.get());
             }
-            outputNodes.push_back(fileNode);
+            outputNodes.push_back(outputNode);
         }
         return outputNodes;
     }
 
     void BuildFileCompiler::compileRule(BuildFile::Rule const& rule) {
         std::vector<std::shared_ptr<FileNode>> cmdInputs = compileInputs(rule.cmdInputs);
+        std::vector<std::shared_ptr<GeneratedFileNode>> orderOnlyInputs; //TODO
         if (rule.forEach) {
             for (auto const& cmdInput : cmdInputs) {
-                std::vector<std::shared_ptr<FileNode>> cmdInputs({ cmdInput });
-                std::vector<std::shared_ptr<GeneratedFileNode>> orderOnlyIn;
-                std::vector<std::shared_ptr<GeneratedFileNode>> outputs = compileOutputs(rule.outputs, cmdInputs);
-                addCommand(rule, cmdInputs, orderOnlyIn);
+                std::vector<std::shared_ptr<FileNode>> cmdInputs_({ cmdInput });
+                std::vector<std::filesystem::path> outputPaths = expandOutputPaths(rule.outputs, cmdInputs_);
+                std::shared_ptr<CommandNode> cmdNode = createCommand(outputPaths);
+                addCommand(rule, cmdNode, cmdInputs_, orderOnlyInputs, outputPaths);
             }
         } else {
-            std::vector<std::shared_ptr<GeneratedFileNode>> orderOnlyInputs;
-            std::vector<std::shared_ptr<GeneratedFileNode>> outputs = compileOutputs(rule.outputs, cmdInputs);
-            addCommand(rule, cmdInputs, orderOnlyInputs);
+            std::vector<std::filesystem::path> outputPaths = expandOutputPaths(rule.outputs, cmdInputs);
+            std::shared_ptr<CommandNode> cmdNode = createCommand(outputPaths);
+            addCommand(rule, cmdNode, cmdInputs, orderOnlyInputs, outputPaths);
         }
-    }
-
-    void BuildFileCompiler::addCommand(
-        BuildFile::Rule const& rule,
-        std::vector<std::shared_ptr<FileNode>> const& cmdInputs,
-        std::vector<std::shared_ptr<GeneratedFileNode>> const& orderOnlyInputs
-    ) {
-        std::shared_ptr<CommandNode> cmdNode;
-
-        std::vector<std::shared_ptr<GeneratedFileNode>> outputs = compileOutputs(rule.outputs, cmdInputs);
-        std::string script = compileScript(rule.script, cmdInputs, orderOnlyInputs, outputs);
-        // TODO: what node name to use when there are no outputs
-        // E.g. use uid and always create new node
-        std::filesystem::path cmdName;
-        if (outputs.empty()) {
-            cmdName = uniqueCmdName(_context->nodes());
-        } else {
-            cmdName = outputs[0]->name() / "__cmd";
-        }
-        std::shared_ptr<Node> node = _context->nodes().find(cmdName);
-        cmdNode = dynamic_pointer_cast<CommandNode>(node);
-        if (cmdNode == nullptr) {
-            cmdNode = std::make_shared<CommandNode>(_context, cmdName);
-        }
-        cmdNode->script(script);
-        // TODO: initialize cmdNode
-        _commands.push_back(cmdNode);
-    }    
+    }  
 }
--- core/BuildFileCompiler.h
+++ core/BuildFileCompiler.h
@@ -28,33 +28,44 @@ namespace YAM {
         }
 
     private:
+        std::vector<std::filesystem::path> expandOutputPaths(
+            BuildFile::Outputs const& outputs,
+            std::vector<std::shared_ptr<FileNode>> const& cmdInputs
+        ) const;
+
+        std::shared_ptr<CommandNode> createCommand(
+            std::vector<std::filesystem::path> const& outputPaths) const;
+
+        void addCommand(
+            BuildFile::Rule const& rule,
+            std::shared_ptr<CommandNode> const& cmdNode,
+            std::vector<std::shared_ptr<FileNode>> const& cmdInputs,
+            std::vector<std::shared_ptr<GeneratedFileNode>> const& orderOnlyInputs,
+            std::vector<std::filesystem::path> const& outputPaths);
+
         std::vector<std::shared_ptr<FileNode>> compileInput(
             BuildFile::Input const& input,
-            std::vector<std::shared_ptr<FileNode>> included);
+            std::vector<std::shared_ptr<FileNode>> included) const;
 
         std::vector<std::shared_ptr<FileNode>> compileInputs(
-            BuildFile::Inputs const& inputs);
+            BuildFile::Inputs const& inputs) const;
 
         std::string compileScript(
             BuildFile::Script const& script,
             std::vector<std::shared_ptr<FileNode>> cmdInputs,
             std::vector<std::shared_ptr<GeneratedFileNode>> orderOnlyInputs,
-            std::vector<std::shared_ptr<GeneratedFileNode>> outputs);
+            std::vector<std::shared_ptr<GeneratedFileNode>> outputs) const;
 
         std::vector<std::shared_ptr<GeneratedFileNode>> compileOutput(
             BuildFile::Output const& output,
-            std::vector<std::shared_ptr<FileNode>> const& inputs);
+            std::vector<std::shared_ptr<FileNode>> const& inputs) const;
 
         std::vector<std::shared_ptr<GeneratedFileNode>> compileOutputs(
-            BuildFile::Outputs const& outputs,
-            std::vector<std::shared_ptr<FileNode>> const& inputs);
+            std::shared_ptr<CommandNode> const& cmdNode,
+            std::vector<std::filesystem::path> const& outputPaths,
+            std::vector<std::shared_ptr<FileNode>> const& inputs) const;
 
         void compileRule(BuildFile::Rule const& rule);
-
-        void addCommand(
-            BuildFile::Rule const& rule,
-            std::vector<std::shared_ptr<FileNode>> const& cmdInputs,
-            std::vector<std::shared_ptr<GeneratedFileNode>> const& orderOnlyInputs);
 
         ExecutionContext* _context;
         std::shared_ptr<DirectoryNode> _baseDir;
